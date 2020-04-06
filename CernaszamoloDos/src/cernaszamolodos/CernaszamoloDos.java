6d976c7bc73e857b170ca2c79917b34ff19e43c1
kpeterr <kpeterr2@gmail.com>
2020-04-06 17:55:27 +0200

    Próbálom megoldani a beolvasást

--- CernaszamoloDos/src/cernaszamolodos/CernaszamoloDos.java
+++ CernaszamoloDos/src/cernaszamolodos/CernaszamoloDos.java
@@ -1,11 +1,33 @@
 package cernaszamolodos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CernaszamoloDos {
 
     public static void main(String[] args) {
         
+        System.out.println("A Cérnszámoló program elindult!");
+        System.out.println("-------------------------------");
+         
+        BufferedReader olvaso =  
+                   new BufferedReader(new InputStreamReader(System.in)); 
+        
+        int oldalszam;
+        
+        try {
+            System.out.println("Add meg az oldalszámot:");
+            oldalszam = Integer.parseInt(olvaso.readLine());
+            //System.out.println(oldalszam);
+        } catch (IOException ex) {
+            System.out.println("Csak számot adhatsz meg" + ex);
+        }
+  
+         
         
         
     }
+    
     
 }
